927cf753c2b09c4f9afb0222b3084c76e6d8905a
mauroepce <mauricioeslainerpc@gmail.com>
2024-07-09 22:31:47 -0400

    register user working

---
+++ api/models/users.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const UserScheme = new mongoose.Schema(
+    {
+        name:{
+            type: String
+        },
+        age:{
+            type: Number
+        },
+        email:{
+            type: String,
+            unique: true
+        },
+        password:{
+            type: String,
+            select: false
+        },
+        role:{
+            type:['user', 'admin'],
+            default: 'user'
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        verificationToken: {
+            type: String
+        },
+        verificationAttempts: {
+            type: Number,
+            default: 0
+        },
+    },
+    {
+        timestamps:true, // createdAt, updatedAt
+        versionKey:false
+    }
+)
+
+module.exports = mongoose.model("users", UserScheme)
